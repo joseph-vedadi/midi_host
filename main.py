8ab28ad96bab23cb214b88ba977e04bb62a96f7b
Joseph <joseph.ve@gmail.com>
2021-12-19 12:14:00 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, send_from_directory
+from flask import jsonify
+import glob
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder="static", static_url_path="")
+CORS(app)
+
+
+@app.route("/all")
+def all():
+    all_midi = glob.glob("./static/midi/*")
+    midi_files = [midi.replace("./static/midi/", "") for midi in all_midi]
+    return jsonify(midi_files)
+
+
+@app.route("/<path:path>")
+def midi(path):
+    return send_from_directory("./static/midi/", path)
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
